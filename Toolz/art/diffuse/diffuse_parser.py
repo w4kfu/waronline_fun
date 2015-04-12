16b0db3dfd389fa1ca451870b0477e901f73edde
w4kfu <gw4kfu@gmail.com>
2015-04-12 02:23:57 +0200

    convert .diffuse 2 .png ; error on some .diffuse files

--- Toolz/art/diffuse/diffuse_parser.py
+++ Toolz/art/diffuse/diffuse_parser.py
@@ -7,6 +7,7 @@ DIFFUSE_SIGNATURE   = 0x464C6469        # 'idLF' / 'FLdi'
 DIFFUSE_VERSION     = 0x01
 
 DIFFUSE_HEADER_SIZE = 0x18
+MIPMAP_DATA_SIZE    = 0x0C
 
 def hexdump(src, length=16):
     FILTER = ''.join([(len(repr(chr(x))) == 3) and chr(x) or '.' for x in range(256)])
@@ -23,11 +24,18 @@ DIFFUSE_Header = construct.Struct("DIFFUSE_Header",
                 construct.ULInt32("version"),               # + 0x04
                 construct.ULInt32("file_size"),             # + 0x08
                 construct.ULInt32("unk_dword_00"),          # + 0x0C
-                construct.ULInt16("unk_word_00"),           # + 0x10
-                construct.ULInt16("unk_word_01"),           # + 0x12
-                construct.ULInt32("unk_dword_01"),          # + 0x14
+                construct.ULInt16("mipmap_width_max"),      # + 0x10
+                construct.ULInt16("mipmap_height_max"),     # + 0x12
+                construct.ULInt32("num_mipmaps"),           # + 0x14
                                )
-
+                               
+MIPMAP_Data = construct.Struct("MIPMAP_Data",
+                construct.ULInt32("offset"),                # + 0x00
+                construct.ULInt16("width"),                 # + 0x04
+                construct.ULInt16("height"),                # + 0x06
+                construct.ULInt32("unk_dword_00"),          # + 0x08
+                               )                               
+                               
 class Buffer:
 
     def __init__(self, buf):
@@ -61,7 +69,6 @@ class Diffuse:
         self.file_size = os.stat(filename).st_size
         self.buf = Buffer(open(filename, "rb").read())
         self.header = DIFFUSE_Header.parse(self.buf.GetBufferSize(DIFFUSE_HEADER_SIZE))
-        #self.header = DIFFUSE_Header.parse(self.buf.GetBufferSize(0x1C))
             
     def is_valid_diffuse(self):
         if self.header['signature'] != DIFFUSE_SIGNATURE:
@@ -81,11 +88,19 @@ def print_diffuse_header(geom_header):
     print "  +0x04 version          : 0x%08X" % geom_header['version']
     print "  +0x08 file_size        : 0x%08X" % geom_header['file_size']
     print "  +0x0C unk_dword_00     : 0x%08X" % geom_header['unk_dword_00']
-    print "  +0x10 unk_word_00      : 0x%04X" % geom_header['unk_word_00']
-    print "  +0x12 unk_word_01      : 0x%04X" % geom_header['unk_word_01']
-    print "  +0x14 unk_dword_01     : 0x%08X" % geom_header['unk_dword_01']
+    print "  +0x10 mipmap_width_max : 0x%04X" % geom_header['mipmap_width_max']
+    print "  +0x12 mipmap_height_max: 0x%04X" % geom_header['mipmap_height_max']
+    print "  +0x14 num_mipmaps      : 0x%08X" % geom_header['num_mipmaps']
     print "-" * 20
 
+def print_mipmap_data(mipmap_data):
+    print "[+] MIPMAP_DATA"
+    print "  +0x00 offset           : 0x%08X" % mipmap_data['offset']
+    print "  +0x04 width            : 0x%04X" % mipmap_data['width']
+    print "  +0x06 height           : 0x%04X" % mipmap_data['height']
+    print "  +0x08 unk_dword_00     : 0x%08X" % mipmap_data['unk_dword_00']
+    print "-" * 20
+    
 # 0x0B * 0x0C    
     
 def main():
@@ -96,22 +111,76 @@ def main():
     if diffuse.is_valid_diffuse() == False:
         sys.exit(1)
     print_diffuse_header(diffuse.header)
-    for i in xrange(0, diffuse.header['unk_dword_01']):
-        buf = diffuse.buf.GetBufferSize(0x4 + 0x2 + 0x2 + 0x4)
+    for i in xrange(0, diffuse.header['num_mipmaps']):
+        mipmap_data = MIPMAP_Data.parse(diffuse.buf.GetBufferSize(MIPMAP_DATA_SIZE))
         saved_pos = diffuse.buf.pos
-        print hexdump(buf)
-        offset = struct.unpack("<I", buf[0:4])[0]
-        print "[+] saved_pos : 0x%08X" % saved_pos
-        print "[+] offset    : 0x%08X" % offset
-        print "[+] com_offset: 0x%08X" % (((i + 2) * 0x0C) + offset)
-        if offset != 0:
-            #diffuse.buf.pos = diffuse.buf.pos - 0x0C + offset
-            diffuse.buf.pos = (((i + 2) * 0x0C) + offset)
-            print hexdump(diffuse.buf.GetBufferSize(0x10))
+        print_mipmap_data(mipmap_data)
+        if mipmap_data['offset'] != 0:
+            diffuse.buf.pos = diffuse.buf.pos - 0x0C + mipmap_data['offset']
+            buf = diffuse.buf.GetBufferSize(mipmap_data['width'] * mipmap_data['height'] / 2)
+            print hexdump(buf[:0x10])
+            decoded = pythonDecodeDXT1(buf)
+            data = []
+            for d in decoded:
+                print len(d)
+                data.append(d)
+            data = ''.join(data)
+            print len(data)
+            import Image
+            img = Image.new('RGB', (mipmap_data['height'], mipmap_data['width']), "black")
+            img.fromstring(data)
+            img.save("test_%d.png" % i, "PNG")
         print "-" * 20
   
         diffuse.buf.pos = saved_pos
-    #print hexdump(diffuse.buf.buf[diffuse.buf.pos:])
     
+# Python-only DXT1 decoder; this is slow!
+# Better to use _dxt1.decodeDXT1 if you can
+# (it's used automatically if available by DdsImageFile)
+def pythonDecodeDXT1(data):
+    # input: one "row" of data (i.e. will produce 4*width pixels)
+    blocks = len(data) / 8  # number of blocks in row
+    out = ['', '', '', '']  # row accumulators
+
+    for xb in xrange(blocks):
+        # Decode next 8-byte block.        
+        c0, c1, bits = struct.unpack('<HHI', data[xb*8:xb*8+8])
+
+        # color 0, packed 5-6-5
+        b0 = (c0 & 0x1f) << 3
+        g0 = ((c0 >> 5) & 0x3f) << 2
+        r0 = ((c0 >> 11) & 0x1f) << 3
+        
+        # color 1, packed 5-6-5
+        b1 = (c1 & 0x1f) << 3
+        g1 = ((c1 >> 5) & 0x3f) << 2
+        r1 = ((c1 >> 11) & 0x1f) << 3
+
+        # Decode this block into 4x4 pixels
+        # Accumulate the results onto our 4 row accumulators
+        for yo in xrange(4):
+            for xo in xrange(4):
+                # get next control op and generate a pixel
+                
+                control = bits & 3
+                bits = bits >> 2
+                if control == 0:
+                    out[yo] += chr(r0) + chr(g0) + chr(b0)
+                elif control == 1:
+                    out[yo] += chr(r1) + chr(g1) + chr(b1)
+                elif control == 2:                                
+                    if c0 > c1:
+                        out[yo] += chr((2 * r0 + r1 + 1) / 3) + chr((2 * g0 + g1 + 1) / 3) + chr((2 * b0 + b1 + 1) / 3)
+                    else:
+                        out[yo] += chr((r0 + r1) / 2) + chr((g0 + g1) / 2) + chr((b0 + b1) / 2)
+                elif control == 3:
+                    if c0 > c1:
+                        out[yo] += chr((2 * r1 + r0 + 1) / 3) + chr((2 * g1 + g0 + 1) / 3) + chr((2 * b1 + b0 + 1) / 3)
+                    else:
+                        out[yo] += '\0\0\0'
+
+    # All done.
+    return tuple(out)
+
 if __name__ == "__main__":
     main()
