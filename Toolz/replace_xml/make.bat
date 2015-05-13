b21f4181d7c4be94351d2d2ed4e5630ad4e97f0c
w4kfu <gw4kfu@gmail.com>
2015-05-13 15:54:19 +0200

    update replace xml DLL

--- Toolz/replace_xml/make.bat
+++ Toolz/replace_xml/make.bat
@@ -1,6 +1,5 @@
-@cl.exe replace_xml.c  /Ox /W3 /GF  /GA /MT /nologo /c /TC /D "_CRT_SECURE_NO_WARNINGS"
+@cl.exe replace_xml.c  /Ox /W3 /GF /GA /MT /nologo /c /TC
 @link replace_xml.obj /dll /release /MANIFEST:NO /MACHINE:IX86 /out:replace_xml.dll
-
 
 del *.obj
 del *.exp
--- Toolz/replace_xml/replace_xml.c
+++ Toolz/replace_xml/replace_xml.c
@@ -7,51 +7,49 @@
 #define FILE_DBG "dbg_replace_xml.txt"
 
 #ifdef __cplusplus
-extern "C"
+    extern "C"
 #endif
 int __stdcall LDE(void* address , DWORD type);
+DWORD (__stdcall *ResumeXML)(void) = NULL;
+char default_ip_addr[32] = "127.0.0.1";
 
-int init_dbg = 0;
-
-void dbg_msg(char *format, ...)
+void DbgMsg(char *format, ...)
 {
-  char buffer[512];
-  va_list args;
-  FILE *fp = NULL;
+    char buffer[512];
+    va_list args;
+    FILE *fp = NULL;
+    static int init_dbg = 0;
 
-  va_start(args, format);
-  memset(buffer, 0, sizeof (buffer));
-  vsprintf(buffer, format, args);
-  if (!init_dbg)
-  {
-		fp = fopen(FILE_DBG, "w");
-		init_dbg = 1;
-  }
-  else
-  {
-		fp = fopen(FILE_DBG, "a");
-  }
-  va_end(args);
-  fprintf(fp, "%s", buffer);
-  fclose(fp);
+    va_start(args, format);
+    memset(buffer, 0, sizeof (buffer));
+    vsprintf_s(buffer, sizeof (buffer) - 1, format, args);
+    if (!init_dbg) {
+        fopen_s(&fp, FILE_DBG, "w");
+        init_dbg = 1;
+    }
+    else {
+        fopen_s(&fp, FILE_DBG, "a");
+    }
+    va_end(args);
+    if (fp) {
+        fprintf(fp, "%s", buffer);
+    }
+    fclose(fp);
 }
 
-BOOL setup_hook(char *module, char *name_export, void *Hook_func, void *trampo, DWORD addr)
+BOOL SetupHook(char *module, char *name_export, void *Hook_func, void *trampo, DWORD addr)
 {
 	DWORD OldProtect;
 	DWORD len = 0;
 	FARPROC Proc;
 	
-	if (addr != 0)
-	{
+	if (addr != 0) {
 		Proc = (FARPROC)addr;
 	}
-	else
-	{
+	else {
 		Proc = GetProcAddress(GetModuleHandleA(module), name_export);
-		if (!Proc)
-		{
-			dbg_msg("[-] GetProcAddress()\n");
+		if (!Proc) {
+			DbgMsg("[-] GetProcAddress() failed: %lu\n", GetLastError());
 			return FALSE;
 		}
 	}
@@ -67,27 +65,24 @@ BOOL setup_hook(char *module, char *name_export, void *Hook_func, void *trampo, 
 	return TRUE;
 }
 
-DWORD (__stdcall *Resume_xml)(void) = NULL;
-
-VOID	ReplaceXML(char *Buffer)
+VOID ReplaceXML(char *Buffer)
 {
-	char new_xml[] = "<?xml version=\"1.0\" encoding=\"utf-8\"?><RootElementOfAnyName><MythLoginServiceConfig>"
+	char FormatXML[] = "<?xml version=\"1.0\" encoding=\"utf-8\"?><RootElementOfAnyName><MythLoginServiceConfig>"
 					 "<Settings><ProductId>2</ProductId><LogLevel>10</LogLevel><MessageTimeoutSecs>20</MessageTimeoutSecs>"
 					 "</Settings><RegionList><Region regionName=\"WAR Live\"><PingServer serverName=\"None\">"
 					 "<Address>0.0.0.0</Address><Port>0</Port></PingServer>"
 					 "<LoginServerList><LoginServer serverName=\"login 1\">"
-					 "<Address>127.0.0.1</Address><Port>18046</Port>"
+					 "<Address>%s</Address><Port>18046</Port>"
 					 "</LoginServer>"
-					 //<LoginServer serverName=\"login 2\">"
-					 //"<Address>127.0.0.1</Address><Port>18047</Port>"
-					 //"</LoginServer>"
 					 "</LoginServerList></Region>"
 					 "</RegionList></MythLoginServiceConfig></RootElementOfAnyName>\x0A\x0A\x00\x00";
-					 
-	memcpy(Buffer, new_xml, sizeof (new_xml));				 
+    char NewXML[540] = {0};                 
+                     
+    sprintf_s(NewXML, sizeof (NewXML) - 1, FormatXML, default_ip_addr);
+	memcpy(Buffer, NewXML, sizeof (NewXML));				 
 }
 
-DWORD __declspec ( naked ) Hook_xml(void)
+DWORD __declspec (naked) HookXML(VOID)
 {
 	__asm
 	{
@@ -99,33 +94,55 @@ DWORD __declspec ( naked ) Hook_xml(void)
 		call	ReplaceXML
 		add		esp, 4
 		popad
-		jmp Resume_xml
+		jmp     ResumeXML
 	}
 }
 
-void Make_Hook(void)
+VOID MakeHook(VOID)
 {
-	Resume_xml = (DWORD(__stdcall *)(void))VirtualAlloc(0, 0x1000, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-	memset(Resume_xml, 0x90, 0x1000);
+	ResumeXML = (DWORD(__stdcall *)(VOID))VirtualAlloc(0, 0x1000, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	memset(ResumeXML, 0x90, 0x1000);
 	/*
-.text:004ACF6F                 mov     ecx, [eax]
-.text:004ACF71                 mov     edi, ecx
-.text:004ACF73                 add     esp, 0Ch
-.text:004ACF76                 mov     [eax], ebx
+        .text:004ACF6F                 mov     ecx, [eax]
+        .text:004ACF71                 mov     edi, ecx
+        .text:004ACF73                 add     esp, 0Ch
+        .text:004ACF76                 mov     [eax], ebx
 	*/
-	setup_hook("WAR", "WAR", &Hook_xml, Resume_xml, 0x004ACF6F);
+	SetupHook("WAR", "WAR", &HookXML, ResumeXML, 0x004ACF6F);
+}
+
+VOID LoadServerIp(VOID)
+{
+    FILE *fp = NULL;
+    char buffer[512] = {0};
+    int ipbytes[4];
+    
+    fopen_s(&fp, "server_ip.txt", "r");
+    if (fp == NULL) {
+        DbgMsg("[+] server_ip.txt not found using default value ip address: %s\n", default_ip_addr);
+        return;
+    }
+    fread(buffer, 1, 15, fp);
+    if (sscanf_s(buffer, "%3d.%3d.%3d.%3d", &ipbytes[3], &ipbytes[2], &ipbytes[1], &ipbytes[0]) != 4) {
+        DbgMsg("[-] Malformated ip in server_ip.txt\n");
+        fclose(fp);
+        return;
+    }
+    sprintf_s(default_ip_addr, sizeof (default_ip_addr) - 1, "%s", buffer);
+    DbgMsg("[+] server_ip.txt found! new default value ip address is : %s\n", default_ip_addr);
+    fclose(fp);
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-	switch(fdwReason)
-	{
-		case DLL_PROCESS_ATTACH:
-			DisableThreadLibraryCalls(GetModuleHandleA(NULL));
-			Make_Hook();
-			break;
-		default:
-			break;
+    switch(fdwReason) {
+        case DLL_PROCESS_ATTACH:
+            DisableThreadLibraryCalls(GetModuleHandleA(NULL));
+            MakeHook();
+            LoadServerIp();
+            break;
+        default:
+            break;
 	}
 	return TRUE;
 }
